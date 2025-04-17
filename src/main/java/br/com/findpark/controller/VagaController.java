a0a0b2d38f982151f8842cd55b061018f35256cc
LuisGustavoUmc <11212101553@alunos.umc.br>
2025-04-17 20:16:45 -0300

    refactor: Modifica @RequestMapping de /vaga para /findpark/vaga

--- src/main/java/br/com/findpark/controller/VagaController.java
+++ src/main/java/br/com/findpark/controller/VagaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/vaga")
+@RequestMapping(value = "/findpark/vaga")
 public class VagaController {
 
     @Autowired
