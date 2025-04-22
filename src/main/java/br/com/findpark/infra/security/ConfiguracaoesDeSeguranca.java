af83662894cd22836466761ac172f702a8c6f10e
LuisGustavoUmc <11212101553@alunos.umc.br>
2025-04-22 19:29:01 -0300

    feat: Adiciona configurações de segurança, filtro e serviço de token JWT

---
+++ src/main/java/br/com/findpark/infra/security/ConfiguracaoesDeSeguranca.java
@@ -0,0 +1,51 @@
+package br.com.findpark.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoesDeSeguranca {
+
+    @Autowired
+    private FiltroSeguranca filtroDeSeguranca;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return  httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "findpark/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "findpark/auth/registrar").permitAll()
+                        .requestMatchers(HttpMethod.POST, "findpark/auth/refresh").permitAll()
+                        .requestMatchers(HttpMethod.POST, "findpark/vaga").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "findpark/vaga").hasAnyRole("ADMIN", "CLIENTE")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(filtroDeSeguranca, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
+
